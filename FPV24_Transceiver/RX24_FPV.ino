2254e73599eab51dd87937370b01736eacc37ab0
Jules <benkemounjules@gmail.com>
2018-07-13 09:44:20 +0000

    Update RX24_FPV.ino

--- FPV24_Transceiver/RX24_FPV.ino
+++ FPV24_Transceiver/RX24_FPV.ino
@@ -9,9 +9,12 @@ RF24 rx (7, 8);
 byte adresses[6] = {"0"}; //meme code couleur que pour le tx
 Servo leftWing; //on déclare l'aileron gauche
 Servo rightWing; //et le droit
-int angleX;
-int angleY;
-int rc[3] = {angleX, angleY}; //en attente de recevoir le paquet
+const int npnMot = 6; //on déclare l'ensemble moteur variateur
+/* ATTENTION:
+ * la valeur gazVal doit bien être entre 0 et 255!
+ */
+int angleX, angleY, gazVal; //de meme que le tx
+int rc[3] = {angleX, angleY, gazVal}; //en attente de recevoir le paquet
 
 void setup() {
   leftWing.attach(9);
@@ -22,32 +25,42 @@ void setup() {
   rx.setPALevel(RF24_PA_MAX); //puissance max
   rx.setDataRate(RF24_250KBPS); //vitesse lente
   rx.openReadingPipe(1, adresses[0]); //on lui donne le code couleur à utiliser
+  rx.openWritingPipe(adresses[0]); //on lui permet d'ecrire
   rx.startListening(); //c'est parti
 
 }
 
 void loop() {
   if(rx.available()) {
+    //on met la LED en constant si on en a une
     while(rx.available()) {
       rx.read(&rc, sizeof(rc)); //on utilise les données recues pour les ailerons
-      if((rc[0] == 0) and (rc[1] != 0)) {
+      analogWrite(npnMot, rc[2]); //variateur
+        if(rc[0] > 0) {
+            //on braque à droite en plus de monter/descendre
+            leftWing(-1*rc[0]); //on baisse l'elevon gauche
+            rightWing(rc[1]); //on leve le droit
+        }
+        
+        else if(rc[0] < 0) {
+            //on vire à gauche monter/descendre
+            rightWing(rc[0]); //on baisse l'elevon droit
+            lefting(rc[1]); //on lève l'autre
+        }
+        
+      else {
         //pitch: on va chercher angleY dans le tableau
         leftWing.write(rc[1]);
         rightWing.write(rc[1]);
-      }
-
-      else if((rc[1] == 0) and (rc[0] !=0)) {
-        //roll: on va chercher angleX dans le tableau
-        rightWing.write(rc[0]);
-        leftWing.write(-1*rc[0]);
-      }
-      delay(15);
-      //reste les directions intermédiaires...
+        }
+       delay(15);
+      //a tester si double elevon braqués = plus maniable ou pas
       
     }
-    //perte de connexion
+    //perte de connexion: on fait clignoter la LED à éclats
      
   }
-    //pas de télécommande à proximité    
-
+    //pas de télécommande à proximité: on fait clignoter la LED à occultations    
+    rx.write(&rc, sizeof(rc)); //on envoie un paquet test toutes les 750ms
+    delay(750);
 }
