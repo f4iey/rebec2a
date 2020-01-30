3d5bdfe3e84f48988232313f56a5c6ffcc0e64e4
Jules <benkemounjules@gmail.com>
2020-01-30 16:23:13 +0000

    migration array data to struct data

--- FPV24_Transceiver/RX24_FPV.ino
+++ FPV24_Transceiver/RX24_FPV.ino
@@ -10,56 +10,67 @@ byte adresses[6] = {"0"}; //meme code couleur que pour le tx
 Servo leftWing; //on déclare l'aileron gauche
 Servo rightWing; //et le droit
 Servo ESC;
+
+struct Package{
+  int angleX;
+  int angleY;
+  int gazVal;
+  int rudVal; //de meme que le tx
+};
+struct Package rc;
 /* ATTENTION:
  * la valeur gazVal doit bien être entre 0 et 1023!
  */
-int angleX, angleY, gazVal, rudVal; //de meme que le tx
-int rc[4] = {angleX, angleY, gazVal, rudVal}; //en attente de recevoir le paquet
 
 void setup() {
   leftWing.attach(9);
   rightWing.attach(10);
   ESC.attach(6);
   //réglages radio
+  SPI.begin();
   rx.begin();
-  rx.setChannel(115); //meme canal que rx
+  rx.setChannel(69); //meme canal que rx
   rx.setPALevel(RF24_PA_MAX); //puissance max
   rx.setDataRate(RF24_250KBPS); //vitesse lente
   rx.openReadingPipe(1, adresses[0]); //on lui donne le code couleur à utiliser
   rx.openWritingPipe(adresses[0]); //on lui permet d'ecrire
-  //rx.startListening(); //c'est parti
-
+  rx.startListening();
 }
 
 void loop() {
   if(rx.available()) {
     //on met la LED en constant si on en a une
     ESC.write(0); //on arme le brushless (procédure de démarrage)
+    delay(15);
     while(rx.available()) {
       rx.read(&rc, sizeof(rc)); //on utilise les données recues pour les ailerons
-      vitesseBrushless(rc[2]); //contrôleur
-        if(rc[0] > 90) { //valeur médiane à check sur les vrais sticks
+      vitesseBrushless(rc.gazVal); //contrôleur
+        if(rc.angleX > 90) { //valeur médiane à check sur les vrais sticks
             //on braque à droite en plus de monter/descendre
-            leftWing.write(map(rc[0], 0, 180, 180, 0)); //on baisse l'elevon gauche
-            rightWing.write(rc[1]); //on leve le droit
+            leftWing.write(map(rc.angleX, 0, 180, 180, 0)); //on baisse l'elevon gauche
+            rightWing.write(rc.angleY); //on leve le droit
         }
         
-        else if(rc[0] < 85) {
+        else if(rc.angleX < 85) {
             //on vire à gauche monter/descendre
-            rightWing.write(rc[0]); //on baisse l'elevon droit
-            leftWing.write(rc[1]); //on lève l'autre
+            rightWing.write(rc.angleX); //on baisse l'elevon droit
+            leftWing.write(rc.angleY); //on lève l'autre
         }
         
       else {
         //pitch: on va chercher angleY dans le tableau
-        leftWing.write(rc[1]);
-        rightWing.write(rc[1]);
+        leftWing.write(rc.angleY);
+        rightWing.write(rc.angleY);
         }
        delay(15); //attend que les servo se mettent en place
       //a tester si double elevon braqués = plus maniable ou pas
       
     }
     //perte de connexion: on fait clignoter la LED à éclats
+    ESC.write(45); //on coupe le moteur
+    delay(15);
+    ESC.write(15); //on fait des bips avec
+    delay(1500);
      
   }
     //pas de télécommande à proximité: on fait clignoter la LED à occultations
